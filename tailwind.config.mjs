d719050811440383bdf371909a23ddda0834847c
Kevin <kevinidj18@gmail.com>
2024-07-30 21:16:10 -0600

    advances

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -17,22 +17,34 @@ export default {
 				josefinsans: ['JosefinSans'],
 				redhat: ['RedHat'],
 				urbanist: ['Urbanist'],
-			}
-		},
-		keyframes: {
-			'fade-in-up': {
-				'0%': {
-					opacity: '0',
-					transform: 'translateY(10px)',
-				},
-				'100%': {
-					opacity: '1',
-					transform: 'translateY(0)',
-				},
 			},
-		},
-		animation: {
-			'fade-in-up': 'fade-in-up 0.5s ease-out',
+			keyframes: {
+				'fade-in-up': {
+					'0%': {
+						opacity: '0',
+						transform: 'translateY(10px)',
+					},
+					'100%': {
+						opacity: '1',
+						transform: 'translateY(0)',
+					},
+				},
+				'fade-in-right': {
+					'0%': {
+						opacity: '0',
+						transform: 'translateX(10px)',
+					},
+					'100%': {
+						opacity: '1',
+						transform: 'translateX(0)',
+					}
+				}
+			},
+			animation: {
+				'fade-in-up': 'fade-in-up 0.5s ease-out',
+				'fade-in-right': 'fade-in-right 0.3s ease-out',
+				'spin-slow': 'spin 3s linear infinite',
+			},
 		},
 	},
 	plugins: [],
